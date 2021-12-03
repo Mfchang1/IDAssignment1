12ae7723de30edf763cfe36ef3cd798028d13a0f
Mfchang1 <S10223397@connect.np.edu.sg>
2021-12-03 22:23:24 +0800

    added javascript

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function scrollTo(selector) {
-    var destination = $(selector);
-    $('html, body').animate({scrollTop: destination.offset().top}, 'slow');
-}
-
-$("nav > ul > li > a").bind("click", function() {
-    scrollTo($(this).attr('href'));
-    $(".overlay").removeClass('overlay-open');
-    $("#overlay-menu").fadeIn(800);
-});
+function submitform(){
+    alert("Thank you for submtting the form, will reply as soon as possible")
+} /* Alerts the user after they clicked on the submit button for the cotact me form*/
